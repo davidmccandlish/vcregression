4c9cab23d0f185eb01309ec660ac9673baa99ce5
juannanzhou <ohioplayers@gmail.com>
2020-08-10 22:13:52 -0400

    initial commit

--- vcregression/vc_hmc.py
+++ vcregression/vc_hmc.py
@@ -25,167 +25,167 @@ parser.add_argument("-intermediate_output", help="output intemediate samples and
 parser.add_argument("-sample_name", help="name of the hmc sample")
 
 
-# import numpy as np
-# import scipy as sp
-# import itertools
-# import sys
-# import time
-# import scipy as sp
-# import itertools
-# import os
-# import math
-# import csv
-# import pandas as pd
-# import random as rd
-# import statistics
-# from scipy.sparse import csr_matrix, dia_matrix, save_npz, load_npz
-# from scipy.optimize import minimize
-# from scipy.special import comb
-# from scipy.spatial.distance import hamming
-# from scipy.sparse.linalg import LinearOperator
-# from scipy.sparse.linalg import cg
-# import arrow
+import numpy as np
+import scipy as sp
+import itertools
+import sys
+import time
+import scipy as sp
+import itertools
+import os
+import math
+import csv
+import pandas as pd
+import random as rd
+import statistics
+from scipy.sparse import csr_matrix, dia_matrix, save_npz, load_npz
+from scipy.optimize import minimize
+from scipy.special import comb
+from scipy.spatial.distance import hamming
+from scipy.sparse.linalg import LinearOperator
+from scipy.sparse.linalg import cg
+import arrow
 
 
-# import vc_regression as vc
+import vc_regression as vc
 
 
-# def str2bool(v):
-#     return v.lower() in ("True", "true", "t", "1")
+def str2bool(v):
+    return v.lower() in ("True", "true", "t", "1")
 
-# start_time = arrow.now().format('YYYY-MM-DD-HH-mm')
-# vc.start_time = start_time
-
-
-# args = parser.parse_args()
-# a = args.a
-# l = args.l
+start_time = arrow.now().format('YYYY-MM-DD-HH-mm')
+vc.start_time = start_time
 
 
-# args.intermediate_output = str2bool(args.intermediate_output)
-
-# if args.name == None:
-#     args.name = "my_project"
-
-# name = args.name
-# outdir = name
-
-# outpath = outdir
-# if not os.path.exists(outpath):
-#     os.makedirs(outpath)
-
-# vc.outpath = outpath
+args = parser.parse_args()
+a = args.a
+l = args.l
 
 
-# if args.sample_name == None:
-#     args.sample_name = start_time
-# vc.sample_name = args.sample_name
+args.intermediate_output = str2bool(args.intermediate_output)
+
+if args.name == None:
+    args.name = "my_project"
+
+name = args.name
+outdir = name
+
+outpath = outdir
+if not os.path.exists(outpath):
+    os.makedirs(outpath)
+
+vc.outpath = outpath
 
 
-# if args.qstart == 'mode':
-#     q_start = np.zeros(a**l)
-# else:
-#     if args.qstart == 'random':
-#         q_start = np.random.normal(loc=0, scale=1, size=a**l)
-#     else:
-#         q_start = np.array(pd.read_csv(args.qpath, sep='\t', header=None)).T
-#         q_start = np.ndarray.flatten(q_start)
+if args.sample_name == None:
+    args.sample_name = start_time
+vc.sample_name = args.sample_name
 
 
-# if a**l > 5000000:
-#     print("sequence space is too big!")
-#     exit()
+if args.qstart == 'mode':
+    q_start = np.zeros(a**l)
+else:
+    if args.qstart == 'random':
+        q_start = np.random.normal(loc=0, scale=1, size=a**l)
+    else:
+        q_start = np.array(pd.read_csv(args.qpath, sep='\t', header=None)).T
+        q_start = np.ndarray.flatten(q_start)
 
 
-# vc.preliminary_preparation(a, l)
+if a**l > 5000000:
+    print("sequence space is too big!")
+    exit()
 
 
-# data = pd.read_csv(args.data, header=None)
-
-# #########
-# babel = ''
-# for i in range(len(data)):
-#     babel += data[0][i]
-
-# alphabet = set(babel)
-
-# AA2N = dict([(sorted(alphabet)[i], i) for i in range(len(alphabet))])
-# N2AA = {v: k for k, v in AA2N.items()}
+vc.preliminary_preparation(a, l)
 
 
-# def seqAA2num(seq):
-#     return [AA2N[seq[i]] for i in range(len(seq))]
+data = pd.read_csv(args.data, header=None)
+
+#########
+babel = ''
+for i in range(len(data)):
+    babel += data[0][i]
+
+alphabet = set(babel)
+
+AA2N = dict([(sorted(alphabet)[i], i) for i in range(len(alphabet))])
+N2AA = {v: k for k, v in AA2N.items()}
 
 
-# def seqnum2AA(seq):
-#     seqAA = [N2AA[seq[i]] for i in range(len(seq))]
-#     return ''.join(seqAA)
-
-# seqs = [seqAA2num(data[0][i]) for i in range(len(data))]
-# tr = np.array([vc.seq2pos(seqs[i]) for i in range(len(seqs))])
-
-# ###########
-# AA = list(AA2N.keys())
-# seqsAll = [''.join(seq) for seq in itertools.product(AA, repeat=l)]
-# vc.seqsAll = seqsAll
-
-# if np.shape(seqs)[1] != l:
-#     print("seqs file dimension incompatible!")
-#     exit()
+def seqAA2num(seq):
+    return [AA2N[seq[i]] for i in range(len(seq))]
 
 
-# ys = np.array(data[1])
-# sig2s = np.array(data[2])
+def seqnum2AA(seq):
+    seqAA = [N2AA[seq[i]] for i in range(len(seq))]
+    return ''.join(seqAA)
 
-# lda_star = pd.DataFrame(np.array(pd.read_csv(
-#     args.lambdas, header=None, index_col=0)))
-# lda_star = np.array(lda_star).flatten()
-# vc.lda_star = lda_star
+seqs = [seqAA2num(data[0][i]) for i in range(len(data))]
+tr = np.array([vc.seq2pos(seqs[i]) for i in range(len(seqs))])
 
-# MAP = np.array(pd.read_csv(args.MAP)['phenotype'])
+###########
+AA = list(AA2N.keys())
+seqsAll = [''.join(seq) for seq in itertools.product(AA, repeat=l)]
+vc.seqsAll = seqsAll
 
-
-# vc.set_data_as_global_parameters(seqs, np.zeros(len(seqs)), sig2s)
-
-
-# ##
-
-# vc.prepare_pos_sampling()
+if np.shape(seqs)[1] != l:
+    print("seqs file dimension incompatible!")
+    exit()
 
 
-# samples, varsample = vc.hamiltonian_monte_carlo(
-#     n_samples=args.n_samples,
-#     m=1,
-#     initial_position=q_start,
-#     tune=args.n_tunes,
-#     initial_step_size=args.step_size,
-#     num_steps=args.n_steps,
-#     max_energy_change=1000.0,
-#     intermediate_output=args.intermediate_output
+ys = np.array(data[1])
+sig2s = np.array(data[2])
 
-# )
+lda_star = pd.DataFrame(np.array(pd.read_csv(
+    args.lambdas, header=None, index_col=0)))
+lda_star = np.array(lda_star).flatten()
+vc.lda_star = lda_star
 
-# samples = MAP + samples
-
-# finish_time = arrow.now().format('YYYY-MM-DD-HH-mm')
-
-# pd.DataFrame(samples.T, index=seqsAll).to_csv(outpath + "/" +
-#                                               args.sample_name + "_hmc_samples.txt", index=seqsAll, header=False)
-
-# pd.DataFrame(varsample.T, index=seqsAll).to_csv(outpath + "/" +
-#                                                 args.sample_name + "_hmc_variance.txt", index=seqsAll, header=False)
-
-# # pd.DataFrame(lda_star).to_csv(outpath + "/" + args.sample_name + "_hmc_input_lambdas.txt", index = None, header = False)
+MAP = np.array(pd.read_csv(args.MAP)['phenotype'])
 
 
-# summary = pd.DataFrame([args.sample_name, args.step_size, vc.step_size, vc.acceptance_ratio, args.n_tunes,
-#                         args.n_steps, args.n_samples, start_time, finish_time,
-#                         arrow.get(finish_time, 'YYYY-MM-DD-HH-mm') -
-#                         arrow.get(start_time, 'YYYY-MM-DD-HH-mm'), lda_star], index=["sample_name", "initial_step_size", "final_step_size", "final_acceptance_ratio",
-#                                                                                      "ntunes", "n_steps", "n_samples", "start_time",
-#                                                                                      "finish_time", "total_time", "input_lambdas"])
+vc.set_data_as_global_parameters(seqs, np.zeros(len(seqs)), sig2s)
 
-# print(summary)
 
-# pd.DataFrame(summary).to_csv(outpath + "/" + args.sample_name +
-#                              "_hmc_summary.txt", index=True, header=False)
+##
+
+vc.prepare_pos_sampling()
+
+
+samples, varsample = vc.hamiltonian_monte_carlo(
+    n_samples=args.n_samples,
+    m=1,
+    initial_position=q_start,
+    tune=args.n_tunes,
+    initial_step_size=args.step_size,
+    num_steps=args.n_steps,
+    max_energy_change=1000.0,
+    intermediate_output=args.intermediate_output
+
+)
+
+samples = MAP + samples
+
+finish_time = arrow.now().format('YYYY-MM-DD-HH-mm')
+
+pd.DataFrame(samples.T, index=seqsAll).to_csv(outpath + "/" +
+                                              args.sample_name + "_hmc_samples.txt", index=seqsAll, header=False)
+
+pd.DataFrame(varsample.T, index=seqsAll).to_csv(outpath + "/" +
+                                                args.sample_name + "_hmc_variance.txt", index=seqsAll, header=False)
+
+# pd.DataFrame(lda_star).to_csv(outpath + "/" + args.sample_name + "_hmc_input_lambdas.txt", index = None, header = False)
+
+
+summary = pd.DataFrame([args.sample_name, args.step_size, vc.step_size, vc.acceptance_ratio, args.n_tunes,
+                        args.n_steps, args.n_samples, start_time, finish_time,
+                        arrow.get(finish_time, 'YYYY-MM-DD-HH-mm') -
+                        arrow.get(start_time, 'YYYY-MM-DD-HH-mm'), lda_star], index=["sample_name", "initial_step_size", "final_step_size", "final_acceptance_ratio",
+                                                                                     "ntunes", "n_steps", "n_samples", "start_time",
+                                                                                     "finish_time", "total_time", "input_lambdas"])
+
+print(summary)
+
+pd.DataFrame(summary).to_csv(outpath + "/" + args.sample_name +
+                             "_hmc_summary.txt", index=True, header=False)
--- vcregression/vc_prep.py
+++ vcregression/vc_prep.py
@@ -8,149 +8,149 @@ parser.add_argument(
     "-cv", help="estimate lambdas using regularization with regularization parameter chosen with 10-fold crossvalidation", default=True)
 
 
-# import numpy as np
-# import scipy as sp
-# import itertools
-# import sys
-# import time
-# import scipy as sp
-# import itertools
-# import os
-# import math
-# import csv
-# import pandas as pd
-# import random as rd
-# import statistics
-# from scipy.sparse import csr_matrix, dia_matrix
-# from scipy.optimize import minimize
-# from scipy.special import comb
-# from scipy.spatial.distance import hamming
-# from scipy.sparse.linalg import LinearOperator
-# from scipy.sparse.linalg import cg
+import numpy as np
+import scipy as sp
+import itertools
+import sys
+import time
+import scipy as sp
+import itertools
+import os
+import math
+import csv
+import pandas as pd
+import random as rd
+import statistics
+from scipy.sparse import csr_matrix, dia_matrix
+from scipy.optimize import minimize
+from scipy.special import comb
+from scipy.spatial.distance import hamming
+from scipy.sparse.linalg import LinearOperator
+from scipy.sparse.linalg import cg
 
-# import vc_regression as vc
-
-
-# def str2bool(v):
-#     return v.lower() in ("True", "true", "t", "1")
+import vc_regression as vc
 
 
-# args = parser.parse_args()
-# args.cv = str2bool(args.cv)
+def str2bool(v):
+    return v.lower() in ("True", "true", "t", "1")
 
 
-# a = args.a
-# l = args.l
+args = parser.parse_args()
+args.cv = str2bool(args.cv)
 
 
-# if args.name == None:
-#     args.name = "my_project"
+a = args.a
+l = args.l
 
 
-# outdir = args.name
+if args.name == None:
+    args.name = "my_project"
 
 
-# if not os.path.exists(outdir):
-#     os.makedirs(outdir)
+outdir = args.name
 
 
-# # QC
-# if a**l > 5000000:
-#     print("sequence space is to big!")
-#     exit()
-
-# vc.preliminary_preparation(a, l)
-
-# data = pd.read_csv(args.data, header=None)
-
-# babel = ''
-# for i in range(len(data)):
-#     babel += data[0][i]
-
-# alphabet = set(babel)
-
-# AA2N = dict([(sorted(alphabet)[i], i) for i in range(len(alphabet))])
-# N2AA = {v: k for k, v in AA2N.items()}
-# AA = list(AA2N.keys())
-# seqsAll = [''.join(seq) for seq in itertools.product(AA, repeat=l)]
-# pd.DataFrame(seqsAll).to_csv(outdir + "/sequences.txt", header=None, index=None)
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
 
 
-# def seqAA2num(seq):
-#     return [AA2N[seq[i]] for i in range(len(seq))]
-# ####
+# QC
+if a**l > 5000000:
+    print("sequence space is to big!")
+    exit()
 
-# seqs = [seqAA2num(data[0][i]) for i in range(len(data))]
-# tr = np.array([vc.seq2pos(seqs[i]) for i in range(len(seqs))])
+vc.preliminary_preparation(a, l)
 
+data = pd.read_csv(args.data, header=None)
 
-# if np.shape(seqs)[1] != l:
-#     print("seqs file dimension incompatible!")
-#     exit()
+babel = ''
+for i in range(len(data)):
+    babel += data[0][i]
 
+alphabet = set(babel)
 
-# ys = np.array(data[1])
-# sig2s = np.array(data[2])
-
-
-# # vc.set_data_as_global_parameters(seqs, ys, sig2s)
-# # vc.construct_A_sparse()
-# # vc.construct_E_sparse()
-
-
-# vc.initialize_computation(seqs, ys, sig2s)
-
-# all_distance_class_Q = all(map(lambda x: x > 0, vc.N_d))
-
-# rhod = vc.rho_d.copy()
-# rhod[0] -= np.mean(sig2s)
-# lambdas_naive = sp.linalg.inv(vc.W_kd.T).dot(rhod)
-# lambdas_naive_positive_Q = all(map(lambda x: x > 0, lambdas_naive))
+AA2N = dict([(sorted(alphabet)[i], i) for i in range(len(alphabet))])
+N2AA = {v: k for k, v in AA2N.items()}
+AA = list(AA2N.keys())
+seqsAll = [''.join(seq) for seq in itertools.product(AA, repeat=l)]
+pd.DataFrame(seqsAll).to_csv(outdir + "/sequences.txt", header=None, index=None)
 
 
-# if args.cv is True:
-#     print("estimating lambdas with regularization (regularization parameter chosen using 10-fold crossvalidation)...")
-#     cv = True
+def seqAA2num(seq):
+    return [AA2N[seq[i]] for i in range(len(seq))]
+####
 
-# elif not all_distance_class_Q:
-#     print("certain distance classes missing from data, estimating lambdas using regularization (regularization parameter chosen with 10-fold crossvalidation)...")
-#     cv = True
-# elif lambdas_naive_positive_Q:
-#     print("estimating lambdas using least squares")
-#     cv = False
-# else:
-#     print("naive lambdas contain nonpositive values, estimating lambdas using regularization (regularization parameter chosen with 10-fold crossvalidation)...")
-#     cv = True
+seqs = [seqAA2num(data[0][i]) for i in range(len(data))]
+tr = np.array([vc.seq2pos(seqs[i]) for i in range(len(seqs))])
 
 
-# betas = 10 ** np.arange(-2, 6, .5)
+if np.shape(seqs)[1] != l:
+    print("seqs file dimension incompatible!")
+    exit()
 
-# rownames = ["order_" + str(k) for k in range(l + 1)]
+
+ys = np.array(data[1])
+sig2s = np.array(data[2])
 
 
-# # Estimate lambdas using 10 fold crossvalidation
-# if cv is True:
+# vc.set_data_as_global_parameters(seqs, ys, sig2s)
+# vc.construct_A_sparse()
+# vc.construct_E_sparse()
 
-#     out = vc.lambdaCV(ys, tr, sig2s, betas, 10)
-#     beta_cv = out[1]
-#     lda = vc.solve_lambda_single_beta(ys, tr, sig2s, beta_cv)
 
-#     print("lambdas = ", str(lda))
+vc.initialize_computation(seqs, ys, sig2s)
+
+all_distance_class_Q = all(map(lambda x: x > 0, vc.N_d))
+
+rhod = vc.rho_d.copy()
+rhod[0] -= np.mean(sig2s)
+lambdas_naive = sp.linalg.inv(vc.W_kd.T).dot(rhod)
+lambdas_naive_positive_Q = all(map(lambda x: x > 0, lambdas_naive))
+
+
+if args.cv is True:
+    print("estimating lambdas with regularization (regularization parameter chosen using 10-fold crossvalidation)...")
+    cv = True
+
+elif not all_distance_class_Q:
+    print("certain distance classes missing from data, estimating lambdas using regularization (regularization parameter chosen with 10-fold crossvalidation)...")
+    cv = True
+elif lambdas_naive_positive_Q:
+    print("estimating lambdas using least squares")
+    cv = False
+else:
+    print("naive lambdas contain nonpositive values, estimating lambdas using regularization (regularization parameter chosen with 10-fold crossvalidation)...")
+    cv = True
+
+
+betas = 10 ** np.arange(-2, 6, .5)
+
+rownames = ["order_" + str(k) for k in range(l + 1)]
+
+
+# Estimate lambdas using 10 fold crossvalidation
+if cv is True:
+
+    out = vc.lambdaCV(ys, tr, sig2s, betas, 10)
+    beta_cv = out[1]
+    lda = vc.solve_lambda_single_beta(ys, tr, sig2s, beta_cv)
+
+    print("lambdas = ", str(lda))
  
-# else:
+else:
 
-# 	lda = lambdas_naive
-# 	print("lambdas = ", str(lda))
+	lda = lambdas_naive
+	print("lambdas = ", str(lda))
  
-# pd.DataFrame(lda, index=rownames).to_csv(outdir + "/lambdas.txt", header=None)
+pd.DataFrame(lda, index=rownames).to_csv(outdir + "/lambdas.txt", header=None)
 
 
-# mks = [comb(l,k)*(a-1)**k for k in range(l+1)]
+mks = [comb(l,k)*(a-1)**k for k in range(l+1)]
 
-# variance_components = np.array([lda[k]*mks[k] for k in range(1,l+1)])
-# variance_components /= np.sum(variance_components)
-# print("variance components = ", str(variance_components))
-# pd.DataFrame(variance_components, index=rownames[1:]).to_csv(outdir + "/variance_components.txt", header=None)
+variance_components = np.array([lda[k]*mks[k] for k in range(1,l+1)])
+variance_components /= np.sum(variance_components)
+print("variance components = ", str(variance_components))
+pd.DataFrame(variance_components, index=rownames[1:]).to_csv(outdir + "/variance_components.txt", header=None)
 
 
 
